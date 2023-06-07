c9d80b82953697dde5afad9e944d61935a479695
aryaHarshit <arya.harshit1964@gmail.com>
2023-06-07 12:50:51 +0530

    Find string in a string

---
+++ Leet/findIndex.py
@@ -0,0 +1,23 @@
+'''
+Given two strings needle and haystack, 
+return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+'''
+
+import re
+class Solution:
+    def strStr(self, haystack, needle):
+        num = needle[0]
+        x = re.search(needle, haystack)
+
+        if x:
+            for i in range(len(haystack)):
+                search_term = haystack[i : i + len(needle)]
+                if search_term == needle:
+                    return i
+        else:
+            return -1
+
+output = Solution()
+result = output.strStr('mississipi', 'issip')
+print(result)  
